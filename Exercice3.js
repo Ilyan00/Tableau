fa6c120761778e31653788ba47999b8b69cc8ce9
Ilyan <146921733+Ilyan00@users.noreply.github.com>
2023-10-07 17:38:11 +0100

    Add files via upload

---
+++ Exercice3.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let names = [];
+
+names.push("Vincent");
+names.push("Paul");
+names.push("Arthur");
+
+for (let i = 0; i < names.length; i++)
+{
+    names[i] += " Va à la péche" ;
+    console.log(names[i]);
+};
